ceb427207eaac7b7d249fbb0bee3ae0348921777
estebancastano <estebin5372@gmail.com>
2024-06-28 16:02:41 -0500

    configurado y testeado tailwind

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import './index.css'
 
 function App() {
   const [count, setCount] = useState(0)
@@ -16,13 +17,13 @@ function App() {
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
-      <h1>Vite + React</h1>
+      <h1 className='text-red-100'>Vite + React</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
         </button>
         <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
+          Edit <code className='bg-green'>src/App.jsx</code> and save to test HMR
         </p>
       </div>
       <p className="read-the-docs">
